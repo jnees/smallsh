f965bbb3a5d3d1c14ab96f01769709b9e7e5e319
Joshua Nees <neesjo@os1.engr.oregonstate.edu>
2021-10-27 20:59:40 -0700

    Add main prompt, read user input, ignore blanks and comments.

--- smallsh.c
+++ smallsh.c
@@ -6,7 +6,43 @@
 #include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
+#include <stdbool.h>
 
-int main(int argc, char* argv[]){
-    return(0);
+struct command{
+    char args[512][2049];
+    FILE redirect_in;
+    FILE redirect_out;
+    bool background;
+};
+
+/**********************************************************************
+    Function: parseCommand(c)
+    
+    Args:
+
+************************************************************************/ 
+void parseCommand(char * input){
+    printf("Parsing command: %s", input);
+
+    
+}
+
+int main(){
+    // Main Prompt
+    char userInput[2049];
+
+    command_prompt:
+    while(1){
+        printf(": ");
+        fgets(userInput, 2048, stdin);
+
+        // Ignore comment
+        if ((userInput[0] == '#') | (userInput[0] == '\n')) {
+            goto command_prompt;
+        } else {
+            parseCommand(userInput);
+        }
+    }
+    
+    
 }
